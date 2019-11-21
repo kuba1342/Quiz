43e5739ce8447a3ead00c62ea3d3620ec2bb068e
kuba1342 <konres44@op.pl>
2019-11-21 13:54:37 +0100

    Add files via upload

---
+++ StartingScreenActivity.java
@@ -0,0 +1,29 @@
+package com.example.kuba.quiz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartingScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starting_screen);
+
+        Button buttonStartQuiz = findViewById(R.id.button_start_quiz);
+        buttonStartQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startQuiz();
+            }
+        });
+    }
+
+    private void startQuiz() {
+        Intent intent = new Intent(StartingScreenActivity.this, QuizActivity.class);
+        startActivity(intent);
+    }
+}
